bd1f1b639268e24d94d2ceb2635f91d98be16d1d
Lana <lanadzuceva@gmail.com>
2021-12-23 23:13:46 +0300

    Добавьте файлы проекта.

---
+++ Balda/FormMain.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Balda
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanelField = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 543);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(776, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanelField
+            // 
+            this.flowLayoutPanelField.Location = new System.Drawing.Point(140, 12);
+            this.flowLayoutPanelField.Name = "flowLayoutPanelField";
+            this.flowLayoutPanelField.Size = new System.Drawing.Size(500, 500);
+            this.flowLayoutPanelField.TabIndex = 1;
+            //this.flowLayoutPanelField.MouseDown += new System.Windows.Forms.MouseEventHandler(this.flowLayoutPanelField_MouseDown);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 655);
+            this.Controls.Add(this.flowLayoutPanelField);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "FormMain";
+            this.Text = "Балда";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelField;
+    }
+}
+
---
+++ Balda/FormMain.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Balda
+{
+    public partial class FormMain : Form
+    {
+        static int size = 10;
+        int cellSize = 100;
+        Label[][] field = new Label[size][];
+        int ind = -1;
+        Label[] massiveLetters = new Label[size];
+        string letter = "";
+        int x = -1, y = -1;
+        string letters = "йцукенгшщзхфывапролджэячсмитьбю";
+        
+        public FormMain()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void label_MouseDown(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)sender;
+            if (e.Button == MouseButtons.Right || letter == "")
+            {
+                label.Text = "";
+                return;
+            }
+            label.BackColor = Color.Bisque;
+            label.Text = letter;
+        }
+
+        private void label_MouseDown_ChooseLetter(object sender, MouseEventArgs e)
+        {
+            Label label = (Label)sender;
+            label.BackColor = Color.Yellow;
+            if (ind != -1)
+            {
+                massiveLetters[ind].BackColor = Color.White;
+            }
+            ind = Array.IndexOf(massiveLetters, label);
+            letter = label.Text;
+        }
+
+        private void flowLayoutPanelField_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            flowLayoutPanelField.BorderStyle = BorderStyle.FixedSingle;
+            flowLayoutPanel1.BorderStyle = BorderStyle.FixedSingle;
+            for (int i = 0; i < size; i++)
+            {
+                field[i] = new Label[size];
+                for (int r = 0; r < size; r++)
+                {
+                    Label label = new Label
+                    {
+                        Width = cellSize,
+                        Height = cellSize,
+                        BorderStyle = BorderStyle.FixedSingle,
+                        TextAlign = ContentAlignment.MiddleCenter,
+                        Font = new Font("Microsoft Sans Serif", 20F)
+                    };
+                    label.MouseDown += new MouseEventHandler(label_MouseDown);
+                    flowLayoutPanelField.Controls.Add(label);
+                    field[i][r] = label;
+                }
+            }
+            Random rnd = new Random();
+            for (int i = 0; i < size; i++)
+            {
+                Label label = new Label
+                {
+                    Width = 70,
+                    Height = 70,
+                    Text = letters[rnd.Next(letters.Length)].ToString(),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Microsoft Sans Serif", 20F),
+                    BorderStyle = BorderStyle.FixedSingle
+                };
+                label.MouseDown += new MouseEventHandler(label_MouseDown_ChooseLetter);
+                flowLayoutPanel1.Controls.Add(label);
+                massiveLetters[i] = label;
+            }
+        }
+        }
+}
